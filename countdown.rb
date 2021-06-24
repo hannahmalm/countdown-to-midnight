aaf61fcfec2565a1189cae633967b0dbb7903331
Hannah Malm <hannahemalm616@gmail.com>
2021-06-23 21:49:11 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+def countdown(second)
+        #start the counter 
+    while second > 0
+          puts "#{second} SECOND(S)!"
+            second -= 1
+            #whenver you have a loop and add return statement, this will stop the loop
+    end
+        return "HAPPY NEW YEAR!"
+    end
+      
+      def countdown_with_sleep(seconds)
+        sleep 5
+end  
 
-def countdown
-end
